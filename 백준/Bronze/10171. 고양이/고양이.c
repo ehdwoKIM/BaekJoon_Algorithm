5a60903ec23d7933fbe5dd1e2e4500a8ebc75d9f
김동재 <87058086+ehdwoKIM@users.noreply.github.com>
2022-04-11 16:14:30 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+printf("\\    /\\ ");
+printf("\n");
+printf(" )  ( ')");
+printf("\n");
+printf("(  /  )");
+printf("\n");
+printf(" \\(__)|");
+return 0;
+}
