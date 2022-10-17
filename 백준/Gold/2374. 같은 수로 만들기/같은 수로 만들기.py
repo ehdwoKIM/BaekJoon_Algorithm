6c7dea881b9437ac364e272f1c44ea2483f6e6d8
김동재 <87058086+ehdwoKIM@users.noreply.github.com>
2022-10-17 15:22:20 +0900

    [Gold IV] Title: 같은 수로 만들기, Time: 108 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/2374. 같은 수로 만들기/같은 수로 만들기.py
@@ -0,0 +1,23 @@
+# 백준 2374
+
+n=int(input())
+stack=[]
+cnt=0
+m=0
+
+for i in range(n):
+    x=int(input())
+    m=max(m,x)
+    
+    if stack:
+        if stack[-1] < x:
+           cnt += x - stack.pop()
+           stack.append(x)
+        else:
+            stack.pop()
+            stack.append(x)
+    else:
+        stack.append(x)
+while stack:
+    cnt+=m-stack.pop()
+print(cnt) 
