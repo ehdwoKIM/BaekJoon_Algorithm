5bbb0688c3f62d388f17cd0a0971af4f21a242af
김동재 <87058086+ehdwoKIM@users.noreply.github.com>
2025-09-26 14:33:07 +0900

    [Gold III] Title: 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 - 1, Time: 732 ms, Memory: 270780 KB -BaekjoonHub

--- 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.java
+++ 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class E{int t,d;E(int t,int d){this.t=t;this.d=d;}}
-    public static void main(String[] args)throws Exception{
+    static class E{int t, d; E(int t,int d){this.t=t; this.d=d;}}
+    public static void main(String[] args) throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int N=Integer.parseInt(br.readLine().trim());
         ArrayList<E> ev=new ArrayList<>(2*N);
@@ -11,17 +11,37 @@ public class Main {
             StringTokenizer st=new StringTokenizer(br.readLine());
             int s=Integer.parseInt(st.nextToken());
             int e=Integer.parseInt(st.nextToken());
-            ev.add(new E(s,+1));
-            ev.add(new E(e,-1));
+            ev.add(new E(s, +1)); 
+            ev.add(new E(e, -1)); 
         }
         ev.sort(Comparator.comparingInt(o->o.t));
-        int cur=0, best=0; long L=-1,R=-1;
-        for(int i=0;i<ev.size()-1;i++){
-            cur += ev.get(i).d; 
-            int t=ev.get(i).t, nt=ev.get(i+1).t;
-            if(cur>best){ best=cur; L=t; R=nt; } 
-            else if(cur==best && L!=-1 && R==t) R=nt;
+
+        int cur=0, best=0;
+        long bestL=-1, bestR=-1;
+        boolean taken=false; 
+        for(int i=0;i<ev.size();){
+            int t = ev.get(i).t;
+            int sum=0;
+            // 같은시각 델타 합치기
+            while(i<ev.size() && ev.get(i).t==t){
+                sum += ev.get(i).d;
+                i++;
+            }
+            cur += sum; 
+            // 다음 이벤트 시각
+            int nextT = (i<ev.size() ? ev.get(i).t : t);
+            if(i<ev.size()){ 
+                if(cur > best){
+                    best = cur;
+                    bestL = t;
+                    bestR = nextT;
+                    taken = true;
+                } else if(cur == best && taken && bestR == t){
+                    bestR = nextT;
+                }
+            }
         }
-        System.out.println(best+"\n"+L+" "+R);
+        System.out.println(best);
+        System.out.println(bestL + " " + bestR);
     }
 }
